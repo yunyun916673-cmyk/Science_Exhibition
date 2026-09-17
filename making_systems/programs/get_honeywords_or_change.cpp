97bb45be20dcb889edb4442337317b85cd332e98
yunyun916673-cmyk <yunyun916673@gmail.com>
2026-09-17 19:30:53 +0800

    Update get_honeywords_or_change.cpp

--- making_systems/programs/get_honeywords_or_change.cpp
+++ making_systems/programs/get_honeywords_or_change.cpp
@@ -9,7 +9,6 @@
 #include "webdist.cpp"
 #include "transing_best64.cpp"
 #include "transing_dive.cpp"
-#include "seclists_rulling.cpp"
 #include "rockyou_rulling.cpp"
 #include "Taiwan_rulling.cpp"
 using ll=long long;
@@ -44,19 +43,18 @@ bool vers_lv(const data& a,const data& b) {return a.L<b.L;}
 bool vers_wd(const data& a,const data& b) {return a.W<b.W;}
 void changing(string S)
 {
-    
     ll sz=(ll)S.size();
     ll pos=sz-1LL;
     string test="";
-    while (pos>=0LL)
+    while(pos>=0LL)
     {
-        if (!isdigit(S[pos])) break;
+        if(!isdigit(S[pos])) break;
         test=S[pos]+test;
         pos--;
         S.pop_back();
     }
-    ll val;
-    if (test!="") val=stoll(test);
+    ll val=0LL;
+    if(test!="") val=stoll(test);
     if(S=="password_length") length=val;
     else if(S=="need_numbers") nums=true;
     else if(S=="no_numbers") nums=false;
@@ -75,14 +73,13 @@ void changing(string S)
 }
 pair<vector<string>,ll> get_honeywords(const string& S,bool is_set)
 {
-    if (is_set) {changing(S);return {{},-2LL};}
+    if(is_set) {changing(S);return {{},-2LL};}
     static mt19937 gen(random_device{}());
     vector<string> temp;
     vector<string> list1=run_best64(S);
     vector<string> list2=run_dive(S);
-    vector<string> list3=run_seclists(S);
-    vector<string> list4=run_rockyou(S);
-    vector<string> list5=run_taiwan(S);
+    vector<string> list3=run_rockyou(S);
+    vector<string> list4=run_taiwan(S);
     auto add_to_temp=[&](const vector<string>& lst)
     {
         for(const string& word:lst)
@@ -93,7 +90,6 @@ pair<vector<string>,ll> get_honeywords(const string& S,bool is_set)
     add_to_temp(list2);
     add_to_temp(list3);
     add_to_temp(list4);
-    add_to_temp(list5);
     vector<data> finall;
     for(string& str:temp)
     {
@@ -117,7 +113,7 @@ pair<vector<string>,ll> get_honeywords(const string& S,bool is_set)
         if(pos==where) answers.push_back(S);
     }
     sort(finall.begin(),finall.end(),vers_wd);
-    for(ll i=0LL;i<val;i++)
+    for(ll i=0LL;i<how_many-val;i++)
     {
         answers.push_back(finall[i].word);
         pos++;
